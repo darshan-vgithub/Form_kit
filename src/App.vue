d29dc226ca69c761d41b565be6a40c6ae2e42f0e
Darshan.V <darshanv487@gmail.com>
2024-04-11 20:03:52 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Registration_Form />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Registration_Form from "../src/components/Registration_Form.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Registration_Form,
+  },
+};
 </script>
 
 <style>
@@ -19,7 +18,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
